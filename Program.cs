5ff76b519ac5810b1f216f4ff8afbb6eff8c78cb
WilliamIV <william.green.iv@gmail.com>
2016-09-27 19:39:03 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPracticeSlide5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 33, 5, 10, 25, 7, 50 }; 
+            Console.WriteLine("The sum of my array is {0}", sumNum(num));
+        }
+
+
+        static int sumNum(int[] num)
+        {
+
+
+            int answer = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                answer += num[i]; 
+            }
+            return answer;
+        }
+    }
+}
